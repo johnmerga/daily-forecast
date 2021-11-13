6561ba358d40938c78d2627972c5903d3c734426
johnmerga <johnmerga@yahoo.com>
2021-11-13 15:21:24 +0300

    minor configuration

--- public/js/app.js
+++ public/js/app.js
@@ -11,21 +11,19 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
   if (!location) {
-     data_1.textContent = "please provide an address"
+    data_1.textContent = "please provide an address";
   }
   data_1.textContent = "Loading..";
   data_2.textContent = "";
-  fetch("/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          // return console.log(data.error);
-          data_1.textContent = data.error;
-        } else {
-          data_1.textContent = data.location;
-          data_2.textContent = data.foreCastData;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        // return console.log(data.error);
+        data_1.textContent = data.error;
+      } else {
+        data_1.textContent = data.location;
+        data_2.textContent = data.foreCastData;
+      }
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const geoCode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+// port configuration
+const port = process.env.PORT || 3000;
+
 // Defining Path
 const publicDirectory = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -107,6 +110,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log(" server started running on port 3000 ");
+app.listen(port, () => {
+  console.log(" server started running on port  " + port);
 });
