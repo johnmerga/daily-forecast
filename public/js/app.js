27ceefd433fb8dea8eec7278d8e79bc03980158d
johnmerga <johnmerga@yahoo.com>
2021-11-13 07:32:50 +0300

    client side files

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+console.log("Client side JS is running");
+
+const weatherForm = document.querySelector("form");
+//
+const search = document.querySelector("input");
+const data_1 = document.querySelector("#data_1");
+const data_2 = document.querySelector("#data_2");
+//
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  if (!location) {
+     data_1.textContent = "please provide an address"
+  }
+  data_1.textContent = "Loading..";
+  data_2.textContent = "";
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          // return console.log(data.error);
+          data_1.textContent = data.error;
+        } else {
+          data_1.textContent = data.location;
+          data_2.textContent = data.foreCastData;
+        }
+      });
+    }
+  );
+});
